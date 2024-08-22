c485a7c45d6561c7821508d339fb479d43cf5f6d
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-08-22 19:50:05 +0600

    Student enroll course UI

--- src/pages/student/OfferedCourse.tsx
+++ src/pages/student/OfferedCourse.tsx
@@ -1,7 +1,8 @@
+import { Button, Col, Row } from "antd";
 import { studentCourse } from "../../redux/fetures/student/studentCourseManagement";
 
 const OfferedCourse = () => {
-  const { data: OfferedCourseData } =
+  const { data: OfferedCourseData, isLoading } =
     studentCourse.useGetAllOfferedCoursesQuery(undefined);
 
   const singleObject = OfferedCourseData?.data?.reduce((acc, item) => {
@@ -10,15 +11,52 @@ const OfferedCourse = () => {
     acc[key].section.push({
       section: item.section,
       _id: item._id,
+      startTile: item.startTime,
+      endTime: item.endTime,
+      days: item.days,
     });
     return acc;
   }, {});
 
-  console.log(Object.values(singleObject ? singleObject : {}));
+  const modifiedData = Object.values(singleObject ? singleObject : {});
 
   return (
     <div>
-      <h2>this is student offered course component</h2>
+      <h2 className="">this is student offered course component</h2>
+
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <Row>
+          {modifiedData.map((item) => (
+            <Col span={24} style={{ border: "solid" }}>
+              <div>
+                <h2>{item?.courseTitle}</h2>
+              </div>
+              <div>
+                {item?.section.map((section) => (
+                  <Row
+                    justify="space-between"
+                    align="middle"
+                    style={{ borderTop: "solid #d4d4d4 2px" }}
+                  >
+                    <Col span={5}>Section : {section.section}</Col>
+                    <Col span={5}>Star Time : {section.startTile}</Col>
+                    <Col span={5}>End Time : {section.endTime}</Col>
+                    <Col span={5}>
+                      Days :{" "}
+                      {section.days.map((day) => (
+                        <span> {day} </span>
+                      ))}
+                    </Col>
+                    <Button>Enroll</Button>
+                  </Row>
+                ))}
+              </div>
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
   );
 };
